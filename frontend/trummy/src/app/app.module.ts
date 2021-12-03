c732727ecd4209941a55a03ba0c5eb82c8227cbc
Gustavo Maldonado <maldonadogustavo55@gmail.com>
2021-12-03 13:29:07 +0100

    Basic public structure

---
+++ frontend/trummy/src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { PublicTopBarComponent } from './public-top-bar/public-top-bar.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopBarComponent,
+    PublicTopBarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    AboutUsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/trummy/src/app/public-top-bar/public-top-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-public-top-bar',
+  templateUrl: './public-top-bar.component.html',
+  styleUrls: ['./public-top-bar.component.css']
+})
+export class PublicTopBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
